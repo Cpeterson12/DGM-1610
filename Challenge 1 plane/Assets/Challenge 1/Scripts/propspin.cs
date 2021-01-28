8ca5075a8413551fae63c46405527f6a29c1408e
Caden <cpete.est2014@gmail.com>
2021-01-28 11:46:14 -0700

    plane

---
+++ Challenge 1 plane/Assets/Challenge 1/Scripts/propspin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propspin : MonoBehaviour
+{
+  // rotates prop
+    public float turnSpeed = 200.0f;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime);
+    }
+}
