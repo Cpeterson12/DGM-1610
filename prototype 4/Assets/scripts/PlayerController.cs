11de7a899026f0162c8f0135151393193b051630
caden <cpete.est2014@gmail.com>
2021-04-01 19:48:04 -0600

    Prototype 4

---
+++ prototype 4/Assets/scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private GameObject focalPoint;
+    private Rigidbody playerRb;
+
+    public bool hasPowerup;
+    public float powerupStrength =16.0f;
+
+    public GameObject powerupIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       playerRb = GetComponent<Rigidbody>();
+       focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, 0.5f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Pickup and remove powerup
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            Debug.Log("POWER");
+            StartCoroutine(PowerupCountdownRutine());
+
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+      if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+      {
+          // Gets the enemy's rigidbody
+          Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+          //Gets the position of the enemy in relation to the player
+          Vector3 awayFromplayer = (collision.gameObject.transform.position - transform.position);
+          // report player collision with the powerup
+          Debug.Log("Player has collided with " + collision.gameObject + "with powerup set to " + hasPowerup);
+          // Kicks the enemy away from the player when they collide with the player.
+          enemyRigidbody. AddForce(awayFromplayer * powerupStrength, ForceMode.Impulse);
+      }
+  }
+  IEnumerator PowerupCountdownRutine()
+  {
+      yield return new WaitForSeconds(7); hasPowerup = false;
+      powerupIndicator.gameObject.SetActive (false);
+  }
+}
---
+++ prototype 4/Assets/scripts/SpawnManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    
+    private float spawnRange = 9;
+
+    public int enemyCount;
+    public int waveNumber =1;
+    public GameObject powerupPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn an Enemy
+       SpawnEnemyWave(waveNumber);
+
+       //Create Powerup for the player to collect
+       Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    void update()
+    {
+        // Find how many enemies are in play
+       enemyCount = FindObjectsOfType<enemy>().Length;
+       //if enemy count gets down to zero, spawn in greater number
+       if(enemyCount == 0)
+       {
+           waveNumber ++;
+           SpawnEnemyWave(waveNumber);
+         //Creates additional Powerups for the player to collect
+         Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+       }
+    }
+
+  // method of Vector3 instead of void because it will return data
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        //returns value to function above
+        return randomPos;
+    }
+   
+   void SpawnEnemyWave(int enemiesToSpawn)
+    {
+       for(int i = 0; i < enemiesToSpawn; i++)
+       {
+          Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+       }
+    }
+}
